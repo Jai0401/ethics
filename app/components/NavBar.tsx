1ba88b7e13705e264534754b25854d05feb0ab62
Jai0401 <21cs3025@rgipt.ac.in>
2024-06-01 19:05:09 +0530

    Add: products card

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,21 +3,21 @@ import Image from "next/image";
 
 export function NavBar() {
     return (
-        <div className="p-4 shadow-md fixed w-full top-0 z-50">
+        <div className="p-4 shadow-md fixed w-full top-0 z-50 backdrop-filter backdrop-blur-lg bg-white bg-opacity-30">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-2xl font-bold">
                     <Link href="/" className="text-black">
-                    <Image src="/logo.png" width={120} height={120} alt="Logo" className="hover:scale-110 transition duration-300"></Image>
+                    <Image src="/logo.png" width={100} height={100} alt="Logo" className="hover:scale-150 transition duration-300"></Image>
                     </Link>
                 </div>
                 <div className="flex space-x-8">
-                    <Link href="/" className="text-gray-600 hover:text-[#ec4899] hover:scale-150 transition duration-300 text-lg font-medium">
+                    <Link href="/" className="text-gray-600 hover:text-[#ec4899] hover:scale-110 transition duration-300 text-lg font-medium">
                         Home
                     </Link>
-                    <Link href="/products" className="text-gray-600 hover:text-[#ec4899] hover:scale-150 transition duration-300 text-lg font-medium">
+                    <Link href="/products" className="text-gray-600 hover:text-[#ec4899] hover:scale-110 transition duration-300 text-lg font-medium">
                         Products
                     </Link>
-                    <Link href="/about-us" className="text-gray-600 hover:text-[#ec4899] hover:scale-150 transition duration-300 text-lg font-medium">
+                    <Link href="/about-us" className="text-gray-600 hover:text-[#ec4899] hover:scale-110 transition duration-300 text-lg font-medium">
                         About
                     </Link>
                 </div>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,21 @@
+import ProductCard from "../components/ProductCard";
+
 export default function Products() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center mt-10 mb-6">Products</h1>
+      <div className="container mx-auto px-4">
+        <div className="flex justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            <ProductCard imageUrl="/4.ELEPHANT.png" imageAlt="product" title="ELEPHANT" description="Elephant Feeding Bottle of size xl" linkUrl="#" linkText="more" />
+            <ProductCard imageUrl="/4.ELEPHANT.png" imageAlt="product" title="ELEPHANT" description="Elephant Feeding Bottle" linkUrl="#" linkText="more" />
+            <ProductCard imageUrl="/4.ELEPHANT.png" imageAlt="product" title="ELEPHANT" description="Elephant Feeding Bottle" linkUrl="#" linkText="more" />
+            <ProductCard imageUrl="/4.ELEPHANT.png" imageAlt="product" title="ELEPHANT" description="Elephant Feeding Bottle" linkUrl="#" linkText="more" />
+            <ProductCard imageUrl="/4.ELEPHANT.png" imageAlt="product" title="ELEPHANT" description="Elephant Feeding Bottle" linkUrl="#" linkText="more" />
+            <ProductCard imageUrl="/4.ELEPHANT.png" imageAlt="product" title="ELEPHANT" description="Elephant Feeding Bottle" linkUrl="#" linkText="more" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
